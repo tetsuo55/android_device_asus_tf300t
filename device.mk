f7efa0033fd517b04f3d14c76953bf33df1624da
tetsuo55 <tetsuo55@users.noreply.github.com>
2016-07-18 18:56:37 +0200

    Update and rename device_tf300t.mk to device.mk
    Move AAPT settings here

--- device.mk
+++ device.mk
@@ -1,10 +1,11 @@
-# Copyright (C) 2011 The Android Open Source Project
+#
+# Copyright (C) 2011-2016 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,6 +16,16 @@
 $(call inherit-product-if-exists, vendor/asus/tf300t/tf300t-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/asus/tf300t/overlay
+
+# Set the physical display size with PRODUCT_AAPT_CONFIG
+# Options are small(~2"-3"), normal(~3"-4"), large(~4"-7"), xlarge(~7" and larger)
+PRODUCT_AAPT_CONFIG := xlarge
+# Set the preferred asset density with PRODUCT_AAPT_PREF_CONFIG
+# See android CCD for valid options
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+# A list of dpis to select prebuilt apk, in precedence order.
+# See android CCD for valid options
+PRODUCT_AAPT_PREBUILT_DPI := mdpi ldpi tvdpi hdpi xhdpi
 
 # Files needed for boot image
 PRODUCT_COPY_FILES += \
